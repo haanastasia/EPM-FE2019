936f77a744bb9a182f46cf9958902f0b35eca211
Anastasia Dolgopolova <haanastasia@yandex.com>
2019-08-25 20:17:28 +0400

    cloned folder

---
+++ lecture_09/src/js/blog/main.js
@@ -0,0 +1,16 @@
+var itemBlog = [
+    {
+        id: 1,
+        rating: "",
+        topics: "",
+        title: "Заголовок 1"
+    }
+];
+
+var topics = ["design","seo","video","css","html","javascript","ecmascript","angular",
+              "react","nodejs","photoshop","social","programming","marketing","business","humor",
+              "promotion","art","creative","technology","style","photo","animation","brand"];
+
+console.log(itemBlog);
+
+ 
