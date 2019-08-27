922a045941bcae5683b6b413f62a6e9d8c59c925
haanastasia <haanastasia@yandex.com>
2019-08-27 14:31:29 +0400

    the output of the function News

--- lecture_09/src/js/blog/main.js
+++ lecture_09/src/js/blog/main.js
@@ -1,9 +1,10 @@
 import { escape, getRand, getAvg, rand } from './helpers'; // вспомогательные функции 
+import { fillPostsData } from './fill'; // функция для вывода новостей
 
-let maxTags = 20; // максимальное кол-во тегов в статье
-let maxRating = 10; // максимальное кол-во оценок в рейтинге
-let maxItems = 30; // максимальное кол-во статей в блоге
-let amountFeed = 3; // кол-во статей отображаемых в ленте новостей
+const MAX_TAGS = 20; // максимальное кол-во тегов в статье
+const MAX_RATING= 10; // максимальное кол-во оценок в рейтинге
+const MAX_ITEMS = 30; // максимальное кол-во статей в блоге
+const AMOUNT_FEED = 3; // кол-во статей отображаемых в ленте новостей
 
 // заполняем массив теги
 var topics = ['design', 'seo', 'video', 'css', 'html', 'javascript', 'ecmascript', 'angular',
@@ -15,35 +16,35 @@ var topics = ['design', 'seo', 'video', 'css', 'html', 'javascript', 'ecmascript
 var itemBlog = [
     {
         id: 1,
-        rating: Array.from({ length: maxRating }, () => getRand(1000, 0)), // заполняем массив, в котором 10 рандомных элементов от 0 до 1000
-        topics: rand(topics, maxTags), // создаем массив из рандомных тегов в рандомном порядке, устанавливаем макс. длину массива
+        rating: Array.from({ length: MAX_RATING }, () => getRand(1001, 0)), // заполняем массив, в котором 10 рандомных элементов от 0 до 1000
+        topics: rand(topics, MAX_TAGS), // создаем массив из рандомных тегов в рандомном порядке, устанавливаем макс. длину массива
         title: escape('Headline > 1') // экранирование для заголовков
     },
     {
         id: 2,
-        rating: Array.from({ length: maxRating }, () => getRand(1000, 0)),
-        topics: rand(topics, maxTags),
+        rating: Array.from({ length: MAX_RATING }, () => getRand(1001, 0)),
+        topics: rand(topics, MAX_TAGS),
         title: escape('Headline & headline')
     },
     {
         id: 3,
-        rating: Array.from({ length: maxRating }, () => getRand(1000, 0)),
-        topics: rand(topics, maxTags),
+        rating: Array.from({ length: MAX_RATING }, () => getRand(1001, 0)),
+        topics: rand(topics, MAX_TAGS),
         title: escape('Headline "Headline"')
     }
 ];
 
 // затем заполняем оставшиеся 27 элементов новостей через цикл
 let j = itemBlog.length + 1;
-do {
+while (j <= MAX_ITEMS) {
     itemBlog.push({
         id: j,
-        rating: Array.from({ length: maxRating }, () => getRand(1000, 0)),
-        topics: rand(topics, maxTags),
+        rating: Array.from({ length: MAX_RATING }, () => getRand(1001, 0)),
+        topics: rand(topics, MAX_TAGS),
         title: 'Headline ' + j
     });
     j++;
-} while (j <= maxItems);
+};
 
 // сортируем массив по рейтингу по убыванию
 itemBlog.sort((a, b) => getAvg(b.rating) - getAvg(a.rating));
@@ -51,17 +52,5 @@ itemBlog.sort((a, b) => getAvg(b.rating) - getAvg(a.rating));
 // выводим массив в консоль для тестирования и проверки
 console.log(itemBlog);
 
-// выводим первые три элемента массива на экран
-let i = 0;
-while (i < amountFeed) {
-    // ищем контейнер для отдельной статьи
-    var item = document.querySelectorAll('.blog__item')[i];
-
-    item.querySelector('.blog__title').innerHTML = itemBlog[i].title; // выполняем поиск элементов по классу в контейнере item и заполняем его
-    itemBlog[i].topics.forEach(function (entry) {
-        item.querySelector('.blog__tags').insertAdjacentHTML('beforeEnd', '<div class="blog-tags__item">' + entry + '</div>');
-    });
-    var rating = getAvg(itemBlog[i].rating).toFixed(2); // находим среднее арифметическое из массива рейтинга, округляем его до сотых, убираем лишние цифры после запятой. 
-    item.querySelector('.blog__rating').innerHTML = '<progress max="100" value="' + rating + '" class="rating__progress"></progress><div class="rating__value" style="width:' + rating + '%" data-value="' + rating + '"></div>';
-    i++;
-}
+// передаем наш отсортированный массив и указываем кол-во выводимых материалов в ленте новостей
+fillPostsData(itemBlog, AMOUNT_FEED);
